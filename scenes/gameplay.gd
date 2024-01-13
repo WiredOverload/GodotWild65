752ea8fe33266fd820cd2fdacc0eda05703b652d
Apples <2352020+apples@users.noreply.github.com>
2024-01-12 23:02:35 -0600

    idk

--- scenes/gameplay.gd
+++ scenes/gameplay.gd
@@ -1,7 +1,20 @@
+class_name Gameplay
 extends Node3D
 
+static var instance: Gameplay
 
-# Called when the node enters the scene tree for the first time.
+
+var time_scale: float = 1.0
+
+
+func _enter_tree() -> void:
+	assert(instance == null)
+	instance = self
+
+func _exit_tree() -> void:
+	assert(instance == self)
+	instance = null
+
 func _ready() -> void:
 	pass # Replace with function body.
 
