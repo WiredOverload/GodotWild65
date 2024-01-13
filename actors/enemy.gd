9ec55d64948e9cb235cfcefbd62dbd11a25b8b24
WiredOverload <mike.everywhere@gmail.com>
2024-01-13 07:41:54 -0600

    enemy kills increase max bag speed

--- actors/enemy.gd
+++ actors/enemy.gd
@@ -2,23 +2,27 @@ extends CharacterBody3D
 
 
 const speed = 1.0
+const weight = 5.0
 var max_health = 1
 var health = max_health
-var target
+var mesh : MeshInstance3D
+var player
 
 func _ready() -> void:
 	velocity = Vector3(randf(), 0, randf())
-	target = $"../Player"
+	player = $"../Player"
+	mesh = $MeshInstance3D
 
 func _physics_process(delta: float) -> void:
 	
-	if target:
-		velocity = (target.position - position).normalized() * speed
+	if player:
+		velocity = (player.position - position).normalized() * speed
 	
 	if health > 0:
 		move_and_slide()
 
 func hit(damage : int):
-	health -= damage
-	var mesh := $MeshInstance3D
-	mesh.mesh.surface_get_material(0).albedo_color = Color.BLACK
+	if health > 0:
+		health -= damage
+		mesh.mesh.surface_get_material(0).albedo_color = Color.BLACK
+		player.max_throw_speed += weight
