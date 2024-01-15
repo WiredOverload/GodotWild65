29da20d17a4339c7c74170b44fadfe4a0e75e2a4
Apples <2352020+apples@users.noreply.github.com>
2024-01-14 20:06:19 -0600

    Test camera

---
+++ autoload/test_camera_3d_manager.gd
@@ -0,0 +1,17 @@
+extends Node
+
+const TEST_CAMERA_3D = "res://autoload/test_camera_3d/test_camera_3d.tscn"
+
+func _ready():
+	if get_viewport().get_camera_3d() != null:
+		return
+	
+	var visual_instances := get_tree().root.find_children("*", "VisualInstance3D", true, false)
+	
+	if visual_instances.is_empty():
+		return
+	
+	await get_tree().process_frame
+	
+	get_tree().root.add_child(load(TEST_CAMERA_3D).instantiate())
+	
