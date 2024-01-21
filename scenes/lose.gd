42c35c12104a4acbb6b89cf90398f5fb386ffac4
Apples <2352020+apples@users.noreply.github.com>
2024-01-21 13:08:48 -0600

    Win

--- scenes/lose.gd
+++ scenes/lose.gd
@@ -4,13 +4,13 @@ extends Node3D
 
 func _process(delta: float) -> void:
 	if Input.is_physical_key_pressed(KEY_ESCAPE):
-		get_tree().change_scene_to_file("res://scenes/gameplay.tscn")
+		get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 	
 	if not press_me.visible:
 		return
 	
 	if Input.is_action_just_pressed("action"):
-		get_tree().change_scene_to_file("res://scenes/gameplay.tscn")
+		get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 
 
 func _on_timer_timeout() -> void:
